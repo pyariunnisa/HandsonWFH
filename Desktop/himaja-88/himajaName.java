72d94000de938d9aaeb9010e9422a9abb4a42645
hi-88 <himajareddy1998@gmail.com>
2020-03-26 18:49:53 +0530

    Name File

---
+++ Desktop/himaja-88/himajaName.java
@@ -0,0 +1,14 @@
+package com.ntt.practice;
+
+import java.util.Scanner;
+
+public class DemoName {
+	public static void main(String[] args) {
+		String name;
+		Scanner sc = new Scanner(System.in);
+		
+		name = sc.nextLine();
+		System.out.println("Name is : "+name);		
+	}
+
+}
