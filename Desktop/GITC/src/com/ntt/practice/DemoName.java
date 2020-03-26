86eb5725f4e012203c2716db3beff801aa5c263b
pyariunnisa <pyariunnisabegum@gmail.com>
2020-03-26 17:00:56 +0530

    My new GITC Rep

---
+++ Desktop/GITC/src/com/ntt/practice/DemoName.java
@@ -0,0 +1,14 @@
+package com.ntt.practice;
+
+import java.util.*;
+
+public class DemoName {
+	public static void main(String[] args) {
+		String name;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enetr name");		
+		name = sc.nextLine();
+		System.out.println("Entered name is : "+name);		
+	}
+
+}
